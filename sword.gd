93d5a09c9f0314ae0c6608bc375b1904fc93e158
chickentuna <chickentuna@hotmail.com>
2026-09-07 20:50:02 +0200

    multiple swords per upgrade

--- sword.gd
+++ sword.gd
@@ -3,12 +3,17 @@ extends Node2D
 @export var angVelocity :float = 4
 
 var swordScene = preload("res://projectile.tscn")
-var sword
+var swords: Array[Node] = []
 var swordAngle = 0
 
-func _ready() -> void:
-	sword = swordScene.instantiate()
+func new_sword() -> Node:
+	var sword := swordScene.instantiate()
 	get_node("/root/Global/hero/CharacterBody2D").add_child(sword)
+	swords.append(sword)
+	return sword
+
+func _ready() -> void:
+	pass
 	
 func get_sword_level() -> int:
 	var weapons := get_node("/root/Global/Weapons")
@@ -16,10 +21,15 @@ func get_sword_level() -> int:
 	return sword_level
 
 func spin_sword(delta: float) -> void:	
-	var sword_level := get_sword_level()
-	swordAngle += delta * (angVelocity * sword_level/5)
-	var angVect := Vector2.from_angle(swordAngle) * 100
-	sword.position = angVect
+	while get_sword_level() > swords.size():
+		new_sword()
+	swordAngle += delta * angVelocity
+	for i in swords.size():
+		var sword := swords[i]
+		var curAngle = swordAngle + i * 2 * PI / swords.size()
+		var angVect := Vector2.from_angle(curAngle) * 100
+		sword.position = angVect
+		
 	
 
 func _physics_process(delta: float) -> void:
